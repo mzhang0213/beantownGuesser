acd9196f3bb9ec5e0cedabfc93cc4beef5853306
Advikar <83314187+AdvikarA@users.noreply.github.com>
2023-04-01 15:57:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from mlxtend.plotting import plot_decision_regions
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
-
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 def knn_comparison(data, k):
     y = data["Hackathon"]
     X = data[["Age", "Exp", "Langs", "Beans", "Glasses", "Git", "Lang", "Start"]]
@@ -31,23 +30,12 @@ k = st.sidebar.slider("Choose value of K", 1, 50)
 
 # Calculate accuracy
 accuracy = knn_comparison(data, k)
+class_names = ["first hackathon", "not first hackathon"]
 
 st.write(accuracy)
+cm = confusion_matrix(y_test, y_pred, labels=class_names)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm,display_labels=class_names)
+disp.plot()
+plt.show()
 
-def plot_metrics(metrics_list):
-    if "Confusion Matrix" in metrics_list:
-        st.subheader("Confusion Matrix")
-        plot_confusion_matrix(model, x_test, y_test, display_labels=   class_names)
-        st.pyplot()
-    if "ROC Curve" in metrics_list:
-        st.subheader("ROC Curve")
-        plot_roc_curve(model, x_test, y_test)
-        st.pyplot()
-    if "Precision-Recall Curve" in metrics_list:
-        st.subheader("Precision-Recall Curve")
-        plot_precision_recall_curve(model, x_test, y_test)
-        st.pyplot()
-metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
-class_names = ["first hackathon", "not first hackathon"]
-plot_metrics(metrics)
 
